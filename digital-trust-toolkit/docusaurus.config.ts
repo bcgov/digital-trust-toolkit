3efa43d47f0a266eef114f80f967aef9cae5d3e2
Akiff Manji <akiff.manji@quartech.com>
2024-08-06 23:22:12 +0000

    chore: cleanup links
    Signed-off-by: Akiff Manji <akiff.manji@quartech.com>

--- digital-trust-toolkit/docusaurus.config.ts
+++ digital-trust-toolkit/docusaurus.config.ts
@@ -60,9 +60,9 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          sidebarId: 'docsSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Docs',
         },
         {
           href: 'https://github.com/bcgov/bc-vcpedia',
