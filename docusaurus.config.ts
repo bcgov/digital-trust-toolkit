f62f1a5d32af84e854c1de433359c4cc34e6517c
Akiff Manji <akiff.manji@quartech.com>
2024-08-12 14:08:37 -0700

    fix: link
    Signed-off-by: Akiff Manji <akiff.manji@quartech.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://bcgov.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/digital-trust-toolkit/',
+  baseUrl: '/digital-trust-toolkit',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
