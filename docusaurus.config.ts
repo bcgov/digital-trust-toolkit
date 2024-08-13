dd49e425201ef14d3e40e6e76989ee179581038e
Kyle Robinson <kyle.robinson@briartech.ca>
2024-08-13 13:49:47 -0700

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,14 +42,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/bcgov/digital-trust-toolkit/tree/main/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/bcgov/digital-trust-toolkit/tree/main/blog/',
         },
         theme: {
           customCss: './src/css/custom.css',
